7123502aca7a0ce9eaf2688c82035a59e1d12fd3
Loong Chan <loongchan@gmail.com>
2014-08-12 18:50:08 +0000

    fixed up some checkstyle warnings for the stuff in entities folder.  Also updated IDE to use spaces for indentation.

--- src/UBC/Exam/MainBundle/Entity/Exam.php
+++ src/UBC/Exam/MainBundle/Entity/Exam.php
@@ -6,192 +6,198 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
+ * This class holds the concept of exam (aka file associated with course)
+ * 
+ * @TOTHINK: should I use compound key?
+ * 
  * @ORM\Entity
  * @ORM\Table(name="exam")
  * @ORM\HasLifecycleCallbacks
  */
-class Exam {
-	
-	/**
-	 * @ORM\Column(type="integer")
-	 * @ORM\Id
-	 * @ORM\GeneratedValue(strategy="AUTO") 
-	 */
-	protected $id;
-	
-	/**
-	 * @ORM\Column(type="string", length=4)
-	 */
-	protected $dept;
-	
-	/**
-	 * @ORM\Column(type="string", length=4)
-	 */
-	protected $number;
-	
-	/**
-	 * @ORM\Column(type="integer")
-	 */
-	protected $year;
-	
-	/**
-	 * @ORM\Column(type="string", length=10)
-	 */
-	protected $term;
-	
-	/**
-	 * @ORM\Column(type="text")
-	 */
-	protected $comments;
-	
-	/**
-	 * 
-	 * @ORM\OneToOne(targetEntity="User")
-	 * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
-	 */
-	protected $uploaded_by;
-	
-	/**
-	 * @ORM\Column(type="integer")
-	 */
-	protected $access_level;
+class Exam 
+{
+    
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO") 
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(type="string", length=4)
+     */
+    protected $dept;
+    
+    /**
+     * @ORM\Column(type="string", length=4)
+     */
+    protected $number;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $year;
+    
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    protected $term;
+    
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $comments;
+    
+    /**
+     * 
+     * @ORM\OneToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected $uploaded_by;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $access_level;
 
-	/**
-	 * @ORM\Column(type="string", length=255, nullable=true)
-	 */
-	public $path;
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    public $path;
 
-	/**
-	 * @var datetime $created
-	 *
-	 * @Gedmo\Timestampable(on="create")
-	 * @ORM\Column(type="datetime")
-	 */
-	private $created;
-	
-	/**
-	 * @var datetime $updated
-	 *
-	 * @Gedmo\Timestampable(on="update")
-	 * @ORM\Column(type="datetime")
-	 */
-	private $modified;
-	
-	/**
-	 * file
-	 * @var unknown
-	 */
-	private $file;
-	
-	/**
-	 * Sets file.
-	 *
-	 * @param UploadedFile $file
-	 */
-	public function setFile(UploadedFile $file = null)
-	{
-		$this->file = $file;
-		
-		// check if we have an old image path
-		if (isset($this->path)) {
-			// store the old name to delete after the update
-			$this->temp = $this->path;
-			$this->path = null;
-		} else {
-			$this->path = 'initial';
-		}
-	}
-	
-	/**
-	 * @ORM\PrePersist()
-	 * @ORM\PreUpdate()
-	 */
-	public function preUpload()
-	{
-		if (null !== $this->getFile()) {
-			// do whatever you want to generate a unique name
-			$filename = sha1(uniqid(mt_rand(), true));
-			$this->path = $filename.'.'.$this->getFile()->guessExtension();
-		}
-	}
+    /**
+     * @var datetime $created
+     *
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+    
+    /**
+     * @var datetime $updated
+     *
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime")
+     */
+    private $modified;
+    
+    /**
+     * file
+     * @var unknown
+     */
+    private $file;
+    
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     * @return void
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        
+        // check if we have an old image path
+        if (isset($this->path)) {
+            // store the old name to delete after the update
+            $this->temp = $this->path;
+            $this->path = null;
+        } else {
+            $this->path = 'initial';
+        }
+    }
+    
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            // do whatever you want to generate a unique name
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->path = $filename.'.'.$this->getFile()->guessExtension();
+        }
+    }
 
-	/**
-	 * @ORM\PostPersist()
-	 * @ORM\PostUpdate()
-	 */
-	public function upload()
-	{
-		// the file property can be empty if the field is not required
-		if (null === $this->getFile()) {
-			return;
-		}
-	
-		// use the original file name here but you should
-		// sanitize it at least to avoid any security issues
-	
-		// move takes the target directory and then the
-		// target filename to move to
-		$this->getFile()->move(
-				$this->getUploadRootDir(),
-				$this->getFile()->getClientOriginalName()
-		);
-	
-		// set the path property to the filename where you've saved the file
-		$this->path = $this->getFile()->getClientOriginalName();
-	
-		// clean up the file property as you won't need it anymore
-		$this->file = null;
-	}
-	
-	/**
-	 * @ORM\PostRemove()
-	 */
-	public function removeUpload()
-	{
-		if ($file = $this->getAbsolutePath()) {
-			unlink($file);
-		}
-	}	
-	
-	/**
-	 * Get file.
-	 *
-	 * @return UploadedFile
-	 */
-	public function getFile()
-	{
-		return $this->file;
-	}
-	
-	public function getAbsolutePath()
-	{
-		return null === $this->path ? null : $this->getUploadRootDir().'/'.$this->path;
-	}
-	
-	public function getWebPath()
-	{
-		return null === $this->path ? null : $this->getUploadDir().'/'.$this->path;
-	}
-	
-	/**
-	 * Please see http://symfony.com/doc/2.3/cookbook/doctrine/file_uploads.html#using-lifecycle-callbacks to remove 
-	 * hard coded __DIR__
-	 * @return string
-	 */
-	protected function getUploadRootDir()
-	{
-		// the absolute directory path where uploaded
-		// documents should be saved
-		return __DIR__.'/../../../../../web/'.$this->getUploadDir();
-	}
-	
-	protected function getUploadDir()
-	{
-		// get rid of the __DIR__ so it doesn't screw up
-		// when displaying uploaded doc/image in the view.
-		return 'uploads/documents';
-	}
-	
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+    
+        // use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+    
+        // move takes the target directory and then the
+        // target filename to move to
+        $this->getFile()->move(
+                $this->getUploadRootDir(),
+                $this->getFile()->getClientOriginalName()
+        );
+    
+        // set the path property to the filename where you've saved the file
+        $this->path = $this->getFile()->getClientOriginalName();
+    
+        // clean up the file property as you won't need it anymore
+        $this->file = null;
+    }
+    
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }   
+    
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+    
+    public function getAbsolutePath()
+    {
+        return null === $this->path ? null : $this->getUploadRootDir().'/'.$this->path;
+    }
+    
+    public function getWebPath()
+    {
+        return null === $this->path ? null : $this->getUploadDir().'/'.$this->path;
+    }
+    
+    /**
+     * Please see http://symfony.com/doc/2.3/cookbook/doctrine/file_uploads.html#using-lifecycle-callbacks to remove 
+     * hard coded __DIR__
+     * @return string
+     */
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../../web/'.$this->getUploadDir();
+    }
+    
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/documents';
+    }
+    
     /**
      * Get id
      *
--- src/UBC/Exam/MainBundle/Entity/User.php
+++ src/UBC/Exam/MainBundle/Entity/User.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace UBC\Exam\MainBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -6,10 +7,12 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
+ * Holds information about the user who logged into the system
+ * 
  * @ORM\Entity
  * @ORM\Table(name="user")
  */
-class User implements UserInterface, \Serializable
+class User implements UserInterface, \Serializable 
 {
     /**
      * @ORM\Column(type="integer")
@@ -17,116 +20,111 @@ class User implements UserInterface, \Serializable
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-
+    
     /**
      * @ORM\Column(type="string", length=25, unique=true)
      */
     private $username;
-
+    
     /**
      * @ORM\Column(type="string", length=64)
      */
     private $password;
-
+    
     /**
      * @ORM\Column(type="string", length=60, unique=true)
      */
     private $email;
-
+    
     /**
      * @ORM\Column(name="is_active", type="boolean")
      */
     private $isActive;
-
+    
     /**
-     * @var datetime $created
      *
-     * @Gedmo\Timestampable(on="create")
-     * @ORM\Column(type="datetime")
+     * @var datetime $created
+     *     
+     *      @Gedmo\Timestampable(on="create")
+     *      @ORM\Column(type="datetime")
      */
     private $created;
     
     /**
-     * @var datetime $updated
      *
-     * @Gedmo\Timestampable(on="update")
-     * @ORM\Column(type="datetime")
+     * @var datetime $updated
+     *     
+     *      @Gedmo\Timestampable(on="update")
+     *      @ORM\Column(type="datetime")
      */
     private $modified;
-    
-    public function __construct()
-    {
+    public function __construct() {
         $this->isActive = true;
         // may not be needed, see section on salt below
         // $this->salt = md5(uniqid(null, true));
     }
-
+    
     /**
      * @inheritDoc
      */
-    public function getUsername()
-    {
+    public function getUsername() {
         return $this->username;
     }
-
+    
     /**
      * @inheritDoc
      */
-    public function getSalt()
-    {
+    public function getSalt() {
         // you *may* need a real salt depending on your encoder
         // see section on salt below
         return null;
     }
-
+    
     /**
      * @inheritDoc
      */
-    public function getPassword()
-    {
+    public function getPassword() {
         return $this->password;
     }
-
+    
     /**
      * @inheritDoc
      */
-    public function getRoles()
-    {
-        return array('ROLE_USER');
+    public function getRoles() {
+        return array (
+                'ROLE_USER' 
+        );
     }
-
+    
     /**
      * @inheritDoc
      */
-    public function eraseCredentials()
-    {
+    public function eraseCredentials() {
     }
-
+    
     /**
+     *
      * @see \Serializable::serialize()
      */
-    public function serialize()
-    {
-        return serialize(array(
-            $this->id,
-            $this->username,
-            $this->password,
-            // see section on salt below
-            // $this->salt,
-        ));
+    public function serialize() {
+        return serialize ( array (
+                $this->id,
+                $this->username,
+                $this->password 
+        // see section on salt below
+        // $this->salt,
+                ) );
     }
-
+    
     /**
+     *
      * @see \Serializable::unserialize()
      */
-    public function unserialize($serialized)
-    {
-        list (
-            $this->id,
-            $this->username,
-            $this->password,
+    public function unserialize($serialized) {
+        list ( $this->id, $this->username, $this->password, 
             // see section on salt below
             // $this->salt
-        ) = unserialize($serialized);
+        ))
+ = unserialize ( $serialized );
     }
 }
