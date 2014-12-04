028f7ecdf525605247467723406189ba9f698990
Pan Luo <pan.luo@ubc.ca>
2014-12-04 13:36:51 -0800

    Add --local option to refresh command to load fixtures locally

--- src/UBC/Exam/MainBundle/Command/SubjectCodeRefreshCommand.php
+++ src/UBC/Exam/MainBundle/Command/SubjectCodeRefreshCommand.php
@@ -16,33 +16,33 @@ class SubjectCodeRefreshCommand extends ContainerAwareCommand {
         $this
             ->setName('exam:subjectcode:refresh')
             ->setDescription('Refresh subject code')
+            ->addOption(
+                'local',
+                null,
+                InputOption::VALUE_NONE,
+                'Load the local SQL instead fetching from SIS API'
+            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $logger = $this->getContainer()->get('logger');
+        if ($input->getOption('local')) {
+            list($subjectCodes, $departmentCodes) = $this->refreshFromLocal($output);
+        } else {
+            list($subjectCodes, $departmentCodes) = $this->refreshFromSIS($output);
+        }
 
-        $logger->info('Calling SIS API to get subject codes...');
-
-        $subjectCodeService = $this->getContainer()->get('sisapi.subject_code');
-        $subjectCodes = $subjectCodeService->getSubjectCodes();
-
-        $logger->info('Calling SIS API to get faculty codes...');
-
-        $departmentCodeService = $this->getContainer()->get('sisapi.department_code');
-        $departmentCodes = $departmentCodeService->getDepartmentCodes();
-
-        $logger->info('Merging codes...');
+        $output->writeln('Merging codes...');
 
         // convert department code array to an associate array for searching by code
         $departmentCodeArray = array();
-        foreach($departmentCodes->codes as $code) {
+        foreach ($departmentCodes->codes as $code) {
             $departmentCodeArray[$code->getCode()] = $code;
         }
 
         $entities = array();
-        foreach($subjectCodes->codes as $code) {
+        foreach ($subjectCodes->codes as $code) {
             $scode = new SubjectFaculty();
             $scode->setUrn($code->getId());
             //$scode->setCode($code->getCode());
@@ -61,11 +61,55 @@ class SubjectCodeRefreshCommand extends ContainerAwareCommand {
             $entities[] = $scode;
         }
 
-        $logger->info('Persisting into database...');
+        $output->writeln('Persisting into database...');
 
         $em = $this->getContainer()->get('doctrine')->getManager();
         $em->getRepository('UBCExamMainBundle:SubjectFaculty')->refresh($entities);
 
-        $logger->info('Done!');
+        $output->writeln('Done!');
+    }
+
+    protected function refreshFromLocal(OutputInterface $logger)
+    {
+        $serializer = $this->getContainer()->get('sisapi.serializer');
+
+        $path = realpath(dirname(__FILE__) . '/../Resources/data');
+
+        $logger->writeln('Loading XM from UBC/Exam/MainBundle/Resources/fixtures/subject_code.xml');
+
+        $subjectCodes = $serializer->deserialize(
+            file_get_contents($path . '/' . 'subject_code.xml'),
+            'UBC\SISAPI\Entity\SubjectCodes',
+            'xml'
+        );
+
+        $logger->writeln('Loading XM from UBC/Exam/MainBundle/Resources/fixtures/department_code.xml');
+
+        $departmentCodes = $serializer->deserialize(
+            file_get_contents($path . '/' . 'department_code.xml'),
+            'UBC\SISAPI\Entity\DepartmentCodes',
+            'xml'
+        );
+
+        return array($subjectCodes, $departmentCodes);
+    }
+
+    /**
+     * @param OutputInterface $logger
+     * @return array
+     */
+    protected function refreshFromSIS(OutputInterface $logger)
+    {
+        $logger->writeln('Calling SIS API to get subject codes...');
+
+        $subjectCodeService = $this->getContainer()->get('sisapi.subject_code');
+        $subjectCodes = $subjectCodeService->getSubjectCodes();
+
+        $logger->writeln('Calling SIS API to get faculty codes...');
+
+        $departmentCodeService = $this->getContainer()->get('sisapi.department_code');
+        $departmentCodes = $departmentCodeService->getDepartmentCodes();
+
+        return array($subjectCodes, $departmentCodes);
     }
 }
