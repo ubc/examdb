d1b3d0ff8f945d499798f29fcaa0dc104812a2fc
Pan Luo <pan.luo@ubc.ca>
2014-12-20 01:23:40 -0800

    Unify environment configs

--- app/config/params.php
+++ app/config/params.php
@@ -1,27 +1,29 @@
 <?php
-//wrapped in if statement so that 
-if (getEnv("OPENSHIFT_MYSQL_DB_HOST") !== false) {
-	$container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
-	$container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
-	$container->setParameter('database_name', getEnv("OPENSHIFT_APP_NAME"));
-	$container->setParameter('database_user', getEnv("OPENSHIFT_MYSQL_DB_USERNAME"));
-	$container->setParameter('database_password', getEnv("OPENSHIFT_MYSQL_DB_PASSWORD"));
-}
-
+// load environment variable configs
 $envs = array(
-    'cas_server',
-    'cas_port',
-    'cas_path',
-    'wiki_base_url',
-    'auth2_username',
-    'auth2_password',
-    'auth2_service_application',
-    'auth2_service_url',
-    'sis_base_url',
+    'OPENSHIFT_MYSQL_DB_HOST' => 'database_host',
+    'OPENSHIFT_MYSQL_DB_PORT' => 'database_port',
+    'OPENSHIFT_APP_NAME' => 'database_name',
+    'OPENSHIFT_MYSQL_DB_USERNAME' => 'database_user',
+    'OPENSHIFT_MYSQL_DB_PASSWORD' => 'database_password',
+    'DB_HOST' => 'database_host',
+    'DB_PORT' => 'database_port',
+    'DB_NAME' => 'database_name',
+    'DB_USERNAME' => 'database_user',
+    'DB_PASSWORD' => 'database_password',
+    'cas_server' => 'cas_server',
+    'cas_port' => 'cas_port',
+    'cas_path' => 'cas_path',
+    'wiki_base_url' => 'wiki_base_url',
+    'auth2_username' => 'auth2_username',
+    'auth2_password' => 'auth2_password',
+    'auth2_service_application' => 'auth2_service_application',
+    'auth2_service_url' => 'auth2_service_url',
+    'sis_base_url' => 'sis_base_url',
 );
 
-array_walk($envs, function($v) use ($container) {
-   if (false !== getEnv($v)) {
-       $container->setParameter($v, getEnv($v));
+array_walk($envs, function($v, $k) use ($container) {
+   if (false !== getEnv($k)) {
+       $container->setParameter($v, getEnv($k));
    }
 });
